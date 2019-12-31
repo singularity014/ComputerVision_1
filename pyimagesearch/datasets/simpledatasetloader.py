ea8c5d4398352d1ec9ea5346a57ffc00673dda27
psharma92 <psharma@naister.com>
2019-12-31 20:53:46 +0100

    created preprocessor and image loader for pipeline.

--- pyimagesearch/datasets/simpledatasetloader.py
+++ pyimagesearch/datasets/simpledatasetloader.py
@@ -25,4 +25,22 @@ class SimpleDatasetLoader:
             # /path/to/dataset/{class}/{image}.jpg
             image = cv2.imread(imagePath)
             label = imagePath.split(os.path.sep)[-2]
+            # check to see if our preprocessors are not None
+            if self.preprocessors is not None:
+                # loop over the preprocessors and apply each to
+                # the image
+                for p in self.preprocessors:
+                    image = p.preprocess(image)
+            # treat our processed image as a "feature vector"
+            # by updating the data list followed by the labels
+            data.append(image)
+            labels.append(label)
+
+            # show an update every `verbose` images
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1,
+                                                      len(imagePaths)))
+
+        # return a tuple of the data and labels
+        return (np.array(data), np.array(labels))
 
